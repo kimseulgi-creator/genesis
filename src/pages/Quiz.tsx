f6d130d00689bbbb4b7f6b7fdff45d4b3f240e2a
kimseulgi <rlatmfrl9@naver.com>
2023-08-08 19:48:50 +0900

    Design: 별자리 맞추기 게임 기본 css 작업

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import { getQuiz } from '../api/quiz';
 import { styled } from 'styled-components';
+import nextIcon from '../images/quizNextIcon.svg';
 
 const Quiz = () => {
-  const { isLoading, isError, data } = useQuery('quiz', getQuiz);
+  const { isLoading, isError, data } = useQuery<any>('quiz', getQuiz);
   const [number, setNumber] = useState<number>(1);
   const clickNextBtnHandler = () => {
     setNumber(number + 1);
@@ -22,16 +23,35 @@ const Quiz = () => {
   return (
     <StSection>
       <h2>별자리맞추기</h2>
-      <p>{`Q${number < 10 ? '0' + number : number}`}</p>
-      <div>
-        <img src={quizImg} />
-        <StOl>
-          {quizData.map(item => {
-            return <li key={item.id}>{item}</li>;
+      {/* <p>{`Q ${number < 10 ? '0' + number : number}`}/5</p> */}
+      <p>{`문제 ${number}`}/5</p>
+      <StQuizWrap>
+        <StImg>
+          <img src={quizImg} />
+        </StImg>
+        <StForm
+          onSubmit={e => {
+            e.preventDefault();
+            clickNextBtnHandler();
+          }}>
+          {quizData.map((item, i) => {
+            return (
+              <div key={item + i}>
+                <input type="radio" id={item} name="checkAnswer" value={item} />
+                <label htmlFor={item}>
+                  {i + 1 + '. '}
+                  {item}
+                </label>
+              </div>
+              // <label htmlFor={item.id}>
+              //   <input type="radio" id={item.id} name="checkAnswer" value={item} />
+              //   {item}
+              // </label>
+            );
           })}
-        </StOl>
-      </div>
-      <button onClick={clickNextBtnHandler}>next</button>
+          <StNextButton type="submit">next</StNextButton>
+        </StForm>
+      </StQuizWrap>
     </StSection>
   );
 };
@@ -40,7 +60,64 @@ export default Quiz;
 const StSection = styled.section`
   background-color: var(--color_black);
   color: var(--color_white);
+  height: 100vh;
+  padding: 60px 0;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  & h2 {
+    font-size: 36px;
+    font-weight: 800;
+    margin-bottom: 30px;
+  }
 `;
-const StOl = styled.ol`
-  list-style: circle;
+
+const StQuizWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 60px;
+  & img {
+    width: 500px;
+    height: 500px;
+  }
+`;
+const StImg = styled.div``;
+const StForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  text-align: left;
+  font-size: 24px;
+  margin-left: 240px;
+  & div {
+    margin: 30px 0;
+    & input {
+      opacity: 0;
+    }
+    & input[type='radio']:checked + label {
+      border-bottom: 3px solid var(--color_purple);
+      padding-bottom: 5px;
+    }
+    & label {
+      cursor: pointer;
+    }
+  }
+`;
+const StNextButton = styled.button`
+  background-image: url(${nextIcon});
+  width: 50px;
+  height: 50px;
+  border: none;
+  background-color: transparent;
+  text-indent: -9999px;
+  position: absolute;
+  top: 50%;
+  right: 240px;
+  transform: translate(-50%, 0px);
+  cursor: pointer;
 `;
