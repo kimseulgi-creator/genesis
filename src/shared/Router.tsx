70bb6898943292f29ca07c0ce454f22fbb7b03fe
kimseulgi <rlatmfrl9@naver.com>
2023-08-10 16:35:46 +0900

    fix: merge 오류 수정

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -5,13 +5,12 @@ import Detail from '../pages/Detail';
 import List from '../pages/List';
 import Calender from '../pages/Calender';
 import Quiz from '../pages/Quiz';
-import Loading1 from '../componants/Loading/Loading1';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Loading1 />} />
+        <Route path="/" element={<Home />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/list" element={<List />} />
         <Route path="/calender" element={<Calender />} />
