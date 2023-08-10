138d7b69cbb03c2798b9564b183190e6e1bfbf9b
muzi55 <muzi5@kakao.com>
2023-08-11 03:44:02 +0900

    Design: Add and modifyanimations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,19 +13,40 @@ import {
   Zoom,
 } from 'react-scroll-motion';
 import './../style/Home.css';
-import { styled } from 'styled-components';
+import { keyframes, styled } from 'styled-components';
 import Constellation from '../componants/Constellation';
 import Button from '../componants/Button';
 import { Spin } from '../anima/Anima';
+import {
+  firstDescription,
+  firstStory,
+  firstTitle,
+  firstperiod,
+  firstseason,
+  secondDescription,
+  secondStory,
+  secondTitle,
+  secondperiod,
+  secondseason,
+} from '../anima/AnimaData';
+import bg from './../../src/images/quiz/quiz_background2.png';
 
 interface Props {}
-const Height = '655vh';
+// const Height = '655vh';
+const Height = '1555vh';
 const Home = (): JSX.Element => {
   return (
     <>
       <div className="bg" style={{ height: Height, paddingTop: '100vh' }}>
         <ScrollContainer>
-          <ScrollPage>
+          <ScrollPage
+          // style={{
+          //   marginTop: '-100vh',
+          //   height: '287vh',
+          //   background: `url(${bg})0 0 no-repeat`,
+          //   backgroundSize: 'cover',
+          // }}
+          >
             <Animator animation={batch(Sticky(), Fade())}>
               <span hidden style={{ fontSize: 30 }}></span>
             </Animator>
@@ -42,15 +63,10 @@ const Home = (): JSX.Element => {
             </Animator>
           </ScrollPage>
 
-          {/* <Constellation /> */}
           <ScrollPage>
-            <Animator animation={batch(Fade(), Sticky(50, 50), Zoom(1.4, 1.2), Spin(1))}>
-              <Constellation />
-            </Animator>
             <Animator animation={batch(Fade(), Sticky(50, 50), Move(0, -12, 0, -25))}>
               <StText>
-                <h2>말발굽 돼지뼈자리</h2>
-                <p>설명</p>
+                <h2>별자리란?</h2>
               </StText>
             </Animator>
           </ScrollPage>
@@ -62,13 +78,30 @@ const Home = (): JSX.Element => {
           </ScrollPage>
 
           <ScrollPage>
-            <Animator animation={batch(Fade(), Sticky(50, 50), Zoom(1.4, 1.2), Spin(1))}>
+            <Animator animation={batch(Fade(), Sticky(50, 50), Zoom(1.4, 1.2), Spin(2))}>
               <Constellation />
             </Animator>
             <Animator animation={batch(Fade(), Sticky(50, 50), Move(0, -12, 0, -25))}>
+              <StTextBox>
+                <p className="descriptionPara">밝은 별을 중심으로</p>
+                <p className="descriptionPara">지구에서 보이는 모습에 따라</p>
+                <p className="descriptionPara">어떤 사물을 연상하도록</p>
+              </StTextBox>
+            </Animator>
+          </ScrollPage>
+
+          <ScrollPage>
+            <Animator animation={Fade()}>
+              <></>
+            </Animator>
+          </ScrollPage>
+
+          <ScrollPage>
+            <Animator animation={batch(Fade(), Sticky(50, 50), MoveIn(0, -12), MoveOut(45, 0))}>
               <StText>
-                <h2>말발굽 돼지뼈자리</h2>
-                <p>설명</p>
+                <p className="hash-Text"># 시대별</p>
+                <p className="hash-Text"># 문화권별</p>
+                <p className="hash-Text"># 88개</p>
               </StText>
             </Animator>
           </ScrollPage>
@@ -83,6 +116,23 @@ const Home = (): JSX.Element => {
 };
 
 export default Home;
+const imgAnima = keyframes`
+0% {
+  transform: rotate(0) scale(0.6) scaleX(-1);
+}
+25%{
+  transform: rotate(12deg) scale(0.6) scaleX(-1);
+}
+50% {
+  transform: rotate(0) scale(0.6) scaleX(-1);
+}
+75%{
+  transform: rotate(12deg) scale(0.6) scaleX(-1);
+}
+100% {
+  transform: rotate(0) scale(0.6) scaleX(-1);
+}
+`;
 const StH1 = styled.div`
   text-align: center;
   font-size: 8vw;
@@ -94,14 +144,80 @@ const StDescriptP = styled.div`
   font-size: 2vw;
   color: #fff;
 `;
-const StText = styled.div`
+const StTextBox = styled.div`
   color: #fff;
-  transform: translateY(-200%);
-  & h2 {
-    font-size: 5vw;
-    margin-bottom: 1rem;
+  font-weight: bold;
+  width: 70vw;
+  font-size: 4vw;
+  position: relative;
+  & .descriptionPara {
+    position: fixed;
+    &:nth-child(1) {
+      top: -15vw;
+    }
+    &:nth-child(2) {
+      top: 0;
+      left: 5vw;
+    }
+    &:nth-child(3) {
+      top: 15vw;
+      right: 0;
+    }
   }
-  & p {
-    font-size: 2vw;
+`;
+const StText = styled.section`
+  color: #fff;
+  position: relative;
+  & .hash-Text {
+    width: 60vw;
+    position: fixed;
+    font-weight: bold;
+    &:nth-child(1) {
+      font-size: 13vw;
+      top: -25vw;
+      /* right: -10vw; */
+      left: -10vw;
+    }
+    &:nth-child(2) {
+      font-size: 9vw;
+      top: 0vw;
+      left: -35vw;
+    }
+    &:nth-child(3) {
+      top: 20vw;
+      font-size: 6vw;
+    }
+  }
+  & .ConstellImg {
+    position: fixed;
+    top: -13vw;
+    left: -15vw;
+    width: 60%;
+    transform: scale(0.6) scaleX(-1);
+    animation: ${imgAnima} 8s infinite linear;
+  }
+  & h2 {
+    font-size: 6vw;
+    margin-bottom: 2rem;
+    font-weight: bold;
+  }
+  & dl {
+    box-sizing: border-box;
+    padding-left: 0.5rem;
+    & dt {
+      margin-bottom: 1rem;
+      font-size: 3vw;
+      font-weight: bold;
+    }
+    & dd {
+      font-size: 2vw;
+      margin-bottom: 2rem;
+    }
+    .dldf {
+      display: flex;
+      & .period {
+        margin-right: 10vw;
+      }
+    }
   }
 `;
